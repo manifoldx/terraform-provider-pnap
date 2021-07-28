ae67fd3bc291bc75a37171df4003d7b7359d3fd4
sam bacha <sam@freighttrust.com>
2021-07-28 16:13:38 -0700

    fix(tf): fmt

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "pnap_server" "Test-Server-1" {
-    hostname = "Test-Server-1"
-    os = "ubuntu/bionic"
-    type = "s1.c1.medium"
-    location = "PHX"
-    ssh_keys = [
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDF9LdAFElNCi7JoWh6KUcchrJ2Gac1aqGRPpdZNowObpRtmiRCecAMb7bUgNAaNfcmwiQi7tos9TlnFgprIcfMWb8MSs3ABYHmBgqEEt3RWYf0fAc9CsIpJdMCUG28TPGTlRXCEUVNKgLMdcseAlJoGp1CgbHWIN65fB3he3kAZcfpPn5mapV0tsl2p+ZyuAGRYdn5dJv2RZDHUZBkOeUobwsij+weHCKAFmKQKtCP7ybgVHaQjAPrj8MGnk1jBbjDt5ws+Be+9JNjQJee9zCKbAOsIo3i+GcUIkrw5jxPU/RTGlWBcemPaKHdciSzGcjWboapzIy49qypQhZe1U75 user2@172.16.1.106"
-    
-    ]
-    #allowed actions are: reboot, reset, powered-on, powered-off, shutdown
-    #action = "powered-on"
+  hostname = "Test-Server-1"
+  os       = "ubuntu/bionic"
+  type     = "s1.c1.medium"
+  location = "PHX"
+  ssh_keys = [
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDF9LdAFElNCi7JoWh6KUcchrJ2Gac1aqGRPpdZNowObpRtmiRCecAMb7bUgNAaNfcmwiQi7tos9TlnFgprIcfMWb8MSs3ABYHmBgqEEt3RWYf0fAc9CsIpJdMCUG28TPGTlRXCEUVNKgLMdcseAlJoGp1CgbHWIN65fB3he3kAZcfpPn5mapV0tsl2p+ZyuAGRYdn5dJv2RZDHUZBkOeUobwsij+weHCKAFmKQKtCP7ybgVHaQjAPrj8MGnk1jBbjDt5ws+Be+9JNjQJee9zCKbAOsIo3i+GcUIkrw5jxPU/RTGlWBcemPaKHdciSzGcjWboapzIy49qypQhZe1U75 user2@172.16.1.106"
+
+  ]
+  #allowed actions are: reboot, reset, powered-on, powered-off, shutdown
+  #action = "powered-on"
 }
